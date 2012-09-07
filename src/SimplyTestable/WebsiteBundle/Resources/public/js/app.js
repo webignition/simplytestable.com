12643c4f324e1df879951cd8f5ba2879aa8e2839
Jon Cram <jon@webignition.net>
2012-09-07 13:11:18 +0100

    landing page recent tests and future development

--- src/SimplyTestable/WebsiteBundle/Resources/public/js/app.js
+++ src/SimplyTestable/WebsiteBundle/Resources/public/js/app.js
@@ -1,4 +1,160 @@
-$(document).ready(function() {  
+$(document).ready(function() {
+    if ($('body.homepage').length) {
+        var getOutputDomainFromWebsiteUrl = function (websiteUrl) {
+            var outputDomain = websiteUrl.replace('http://', '').replace('https://', '')
+            
+            if (outputDomain.charAt(outputDomain.length - 1) == '/') {
+                outputDomain = outputDomain.substr(0, outputDomain.length - 1);
+            }
+            
+            return outputDomain;
+        };
+        
+        var getTotalTaskCount = function (taskCountByState) {
+            var totalTaskCount = 0;
+            
+            for (var stateName in taskCountByState) {
+                if (taskCountByState.hasOwnProperty(stateName)) {
+                    totalTaskCount += taskCountByState[stateName];
+                }
+            }
+            
+            return totalTaskCount;
+        };
+        
+        var getQueuedTaskCount = function (taskCountByState) {
+            return taskCountByState['queued-for-assignment'] + taskCountByState['queued'];
+        };  
+        
+        var getFinishedTaskCount = function (taskCountByState) {
+            return taskCountByState['completed'] + taskCountByState['cancelled'];
+        };  
+        
+        var getCompletionPercent = function (taskCountByState) {
+            if (taskCountByState['completed'] == 0) {
+                return 0;
+            }
+            
+            var finishedTaskCount = taskCountByState['completed'] + taskCountByState['cancelled'];
+            var totalTaskCount = getTotalTaskCount(taskCountByState);
+            
+            if (finishedTaskCount == totalTaskCount) {
+                return 100;
+            }            
+            
+            return ((finishedTaskCount / totalTaskCount) * 100).toFixed(1);
+        };
+        
+        var getStateIcon = function (state) {
+            if (state == 'new') {
+                return 'icon-off';
+            }
+            
+            if (state == 'queued') {
+                return 'icon-off';
+            }
+            
+            if (state == 'preparing') {
+                return 'icon-cog';
+            }
+            
+            if (state == 'in-progress') {
+                return 'icon-play-circle';
+            }
+
+            if (state == 'completed') {                
+                return 'icon-bar-chart';
+            }
+            
+            if (state == 'cancelled') {
+                return 'icon-bar-chart';
+            }
+        };
+        
+        var getStateLabelClass = function (state) {
+            if (state == 'new') {
+                return '';
+            }
+            
+            if (state == 'queued') {
+                return 'info';
+            }
+            
+            if (state == 'preparing') {
+                return 'warning';
+            }
+            
+            if (state == 'in-progress') {
+                return 'warning';
+            }
+
+            if (state == 'completed') {                
+                return 'success';
+            }
+            
+            if (state == 'cancelled') {
+                return 'success';
+            }
+        };        
+        
+        var updateRecentTests = function (data) {
+           var recentSiteTestsList = $('#recent-site-tests-list');
+           recentSiteTestsList.html('');
+            
+            $(data).each(function () {
+                
+                var siteListItem = $('<div class="site span4" />').append(
+                    $('<div class="wrapper" />').append(
+                        '<a class="url" href="'+this.website+'">'+getOutputDomainFromWebsiteUrl(this.website)+'</a>'
+                    ).append(
+                        $('<span class="results" />').append(
+                            $('<a href="http://gears.simplytestable.com/'+this.website+'/'+this.id+'/results/">').append(
+                                $('<span class="badge badge-'+getStateLabelClass(this.state)+'"> <i class="icon state-icon '+getStateIcon(this.state)+'"></i> #'+this.id+' <i class="icon action-icon icon-caret-right"></i> </span>')
+                            )
+                        )
+                    ).append(
+                        $('<div class="row-fluid meta">').append(
+                            $(' <div class="span5 total">').append(
+                                '<span class="test-count">'+getTotalTaskCount(this.task_count_by_state)+'</span><span class="subtext">tests overall</span>'
+                            )
+                        ).append(
+                            $(' <div class="span3 total">').append(
+                                '<span class="test-count">'+getCompletionPercent(this.task_count_by_state)+'<span class="percent">%</span></span><span class="subtext">finished</span>'
+                            )
+                        ).append(
+                            $('<div class="span4 detail">').append(
+                                '<div><span class="queued-count figure">'+getQueuedTaskCount(this.task_count_by_state)+'</span> <span class="caption">queued</span></div>'
+                            ).append(
+                                '<div><span class="in-progress-count figure">'+this.task_count_by_state['in-progress']+'</span> <span class="caption">in progress</span></div>'
+                            ).append(
+                                '<div><span class="finished-count figure">'+getFinishedTaskCount(this.task_count_by_state)+'</span> <span class="caption">finished</span></div>'
+                            )
+                        )
+                    )
+                );
+
+                recentSiteTestsList.append(siteListItem);
+            });            
+        };
+        
+        var requestRecentTestData = function () {
+            $.get('/core-application-proxy/?url=http://app.simplytestable.com/tests/list/3/', function(data) {
+                updateRecentTests(data);
+            }, "json");               
+        };
+
+
+        $.get('/core-application-proxy/?url=http://app.simplytestable.com/tasks/HTML%20validation/completed/count/', function(data) {
+            $('#html-validation-completed-count').text(data);
+        }, "json");
+        
+        requestRecentTestData();
+
+        window.setInterval(function () {
+            requestRecentTestData();
+        }, 1000);
+    }   
+    
     
     $('body.homepage .carousel').carousel('pause');
     
@@ -84,7 +240,7 @@ $(document).ready(function() {
         callback: function () {
             $('#tweet').html(
                 $('#tweet ul li').html()
-            );
+                );
             
             $('#tweet').animate({
                 'opacity':1
