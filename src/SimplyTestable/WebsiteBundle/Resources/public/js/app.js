bc2bf557409b21c2ed2b97e2cddb3e2eb6dee7c9
Jon Cram <jon@webignition.net>
2012-09-10 20:11:03 +0100

    Reimplement roadmap timeline for better IE rendering, currently without indicators

--- src/SimplyTestable/WebsiteBundle/Resources/public/js/app.js
+++ src/SimplyTestable/WebsiteBundle/Resources/public/js/app.js
@@ -159,75 +159,75 @@ $(document).ready(function() {
     $('body.homepage .carousel').carousel('pause');
     
     $('body.roadmap .timeline').each(function () {
-        var futureEvents = [];
-        var pastEvents = [];
-        var currentEvents = [];
-        var now = new Date();        
-        
-        var isToday = function (eventTime) {
-            if (eventTime.getDate() != now.getDate()) {
-                return false;
-            }
-            
-            if (eventTime.getDay() != now.getDay()) {
-                return false;
-            }
-
-            if (eventTime.getFullYear() != now.getFullYear()) {
-                return false;
-            }
-            
-            return true;
-        };
-        
-        var isFuture = function (eventTime) {
-            return eventTime.getTime() > now.getTime();
-        };
-        
-        $('.event', this).each(function () {
-            var event = $(this);
-            var eventTime = new Date($('time', event).attr('datetime'));
-            
-            if (isToday(eventTime)) {
-                currentEvents.push(event);
-                event.addClass('next');
-            } else if (isFuture(eventTime)) {
-                futureEvents.push(event);
-                event.addClass('future');
-            } else {  
-                pastEvents.push(event);
-                event.addClass('past');
-            }
-        });
-        
-        if (currentEvents.length === 0) {
-            if (futureEvents.length > 0) {
-                futureEvents[futureEvents.length - 1].removeClass('future').addClass('next');
-            } else if (pastEvents.length > 0) {
-                pastEvents[0].removeClass('future').addClass('next');
-            }            
-        }
-        
-        $('.next', this).each(function () {
-            var event = $(this);
-            var time = $('time', event);
-            
-            time.after('<span class="indicator"><i class="icon-star highlighted"></i><i class="icon-star-empty"></i></div>');
-        });        
-        
-        $('.past', this).each(function () {
-            var event = $(this);
-            var time = $('time', event);
-            
-            time.after('<span class="indicator"><i class="icon-ok highlighted"></i></div>');
-        });        
-        
-        $('.future', this).each(function () {
-            var event = $(this);
-            var time = $('time', event);
-            
-            time.after('<span class="indicator"><i class="icon-off highlighted"></i></div>');
-        });        
+//        var futureEvents = [];
+//        var pastEvents = [];
+//        var currentEvents = [];
+//        var now = new Date();        
+//        
+//        var isToday = function (eventTime) {
+//            if (eventTime.getDate() != now.getDate()) {
+//                return false;
+//            }
+//            
+//            if (eventTime.getDay() != now.getDay()) {
+//                return false;
+//            }
+//
+//            if (eventTime.getFullYear() != now.getFullYear()) {
+//                return false;
+//            }
+//            
+//            return true;
+//        };
+//        
+//        var isFuture = function (eventTime) {
+//            return eventTime.getTime() > now.getTime();
+//        };
+//        
+//        $('.event', this).each(function () {
+//            var event = $(this);
+//            var eventTime = new Date($('time', event).attr('datetime'));
+//            
+//            if (isToday(eventTime)) {
+//                currentEvents.push(event);
+//                event.addClass('next');
+//            } else if (isFuture(eventTime)) {
+//                futureEvents.push(event);
+//                event.addClass('future');
+//            } else {  
+//                pastEvents.push(event);
+//                event.addClass('past');
+//            }
+//        });
+//        
+//        if (currentEvents.length === 0) {
+//            if (futureEvents.length > 0) {
+//                futureEvents[futureEvents.length - 1].removeClass('future').addClass('next');
+//            } else if (pastEvents.length > 0) {
+//                pastEvents[0].removeClass('future').addClass('next');
+//            }            
+//        }
+//        
+//        $('.next', this).each(function () {
+//            var event = $(this);
+//            var time = $('time', event);
+//            
+//            time.after('<span class="indicator"><i class="icon-star highlighted"></i><i class="icon-star-empty"></i></div>');
+//        });        
+//        
+//        $('.past', this).each(function () {
+//            var event = $(this);
+//            var time = $('time', event);
+//            
+//            time.after('<span class="indicator"><i class="icon-ok highlighted"></i></div>');
+//        });        
+//        
+//        $('.future', this).each(function () {
+//            var event = $(this);
+//            var time = $('time', event);
+//            
+//            time.after('<span class="indicator"><i class="icon-off highlighted"></i></div>');
+//        });        
     });
         
     getTwitters('footer-tweet', { 
