775717321431c99236a883417528962617709738
Jon Cram <jon@webignition.net>
2012-10-14 16:43:33 +0100

    Reenable recent test refresh

--- src/SimplyTestable/WebsiteBundle/Resources/public/js/app.js
+++ src/SimplyTestable/WebsiteBundle/Resources/public/js/app.js
@@ -213,9 +213,9 @@ $(document).ready(function() {
         
         requestRecentTestData();
 
-//        window.setInterval(function () {
-//            requestRecentTestData();
-//        }, 1000);
+        window.setInterval(function () {
+            requestRecentTestData();
+        }, 1000);
     }   
     
     
