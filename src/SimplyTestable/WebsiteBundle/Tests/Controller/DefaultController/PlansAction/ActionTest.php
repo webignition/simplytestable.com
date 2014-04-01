cb299daa7780199fea71e2506577dc1dfdb392ed
Jon Cram <jon@webignition.net>
2014-04-01 12:57:18 +0100

    Fix plans page, add minimal tests

---
+++ src/SimplyTestable/WebsiteBundle/Tests/Controller/DefaultController/PlansAction/ActionTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SimplyTestable\WebsiteBundle\Tests\Controller\DefaultController\PlansAction;
+
+use SimplyTestable\WebsiteBundle\Tests\Controller\DefaultController\ActionTest as BaseActionTest;
+
+class ActionTest extends BaseActionTest {    
+    
+    public function setUp() {
+        parent::setUp();
+        $this->setHttpFixtures($this->getHttpFixtures($this->getFixturesDataPath($this->getName() . '/HttpResponses')));
+    }     
+    
+    protected function getActionName() {
+        return 'plansAction';
+    }
+    
+    public function testPageIsRendered() {        
+        $this->performActionTest(array(
+            'statusCode' => 200
+        ));
+    }
+}
