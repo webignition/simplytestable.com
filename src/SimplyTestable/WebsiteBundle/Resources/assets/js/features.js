7e28173f239011ad7c634b11571f244fffb40241
Jon Cram <jon@webignition.net>
2014-07-31 17:14:37 +0100

    new features page v 0.00001

---
+++ src/SimplyTestable/WebsiteBundle/Resources/assets/js/features.js
@@ -0,0 +1,63 @@
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+            var hashValue = this.hash.slice(1);
+
+            if (target.length) {
+                var offset = $('body').is('.upper-nav-affix') ? 300 : 298;
+
+                $('html,body').animate({
+                    scrollTop: (target.offset().top) - offset
+                }, 400, function () {
+                    window.location.hash = hashValue;
+                });
+
+                return false;
+            }
+        }
+    });
+
+    if ($(window.location.hash).length) {
+        var target = $(window.location.hash);
+
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: (target.offset().top) - 300
+            }, 400);
+        }
+    }
+
+    $('#upper-nav').on('affix.bs.affix', function (event) {
+        $('body').addClass('upper-nav-affix').removeClass('upper-nav-affix-top');
+    }).on('affix-top.bs.affix', function (event) {
+        $('body').removeClass('upper-nav-affix').addClass('upper-nav-affix-top');
+    }).on('activate.bs.scrollspy', function (event) {
+        console.log('cp01');
+
+//        if ($('a', event.target).attr('href') == '#invites') {
+//            if (input.stFormHelper().isEmpty() || input.stFormErrorHelper().hasError()) {
+//                input.stFormHelper().select();
+//            }
+//        }
+    });
+
+    $('#upper-nav-fundamental-testing em').click(function () {
+        var hashValue = $(this).attr('data-target');
+        var target = $(hashValue);
+
+        if (target.length) {
+            var offset = $('body').is('.upper-nav-affix') ? 300 : 298;
+
+            $('html,body').animate({
+                scrollTop: (target.offset().top) - offset
+            }, 400, function () {
+                window.location.hash = hashValue.slice(1);
+            });
+
+            return false;
+        }
+    });
+});
