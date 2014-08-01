4ec75a9baaa46a60138b5e3e6c8d142a50322ed8
Jon Cram <jon@webignition.net>
2014-08-01 18:05:41 +0100

    Features page sm layout

--- src/SimplyTestable/WebsiteBundle/Resources/assets/js/features.js
+++ src/SimplyTestable/WebsiteBundle/Resources/assets/js/features.js
@@ -1,4 +1,63 @@
 $(function() {
+    var displayClasses = ['lg', 'md', 'sm', 'xs'];
+
+    var deriveDisplaySizeClass = function () {
+        for (var classIndex = 0; classIndex < displayClasses.length; classIndex++) {
+            var current = $('#' + displayClasses[classIndex]);
+            if (current.css('display') !== 'none') {
+                return displayClasses[classIndex];
+            }
+        }
+    };
+
+    $('body').addClass('display-' + deriveDisplaySizeClass()).attr('data-display-class', deriveDisplaySizeClass().replace('display-', ''));
+
+    $(window).on('resize', function () {
+        var body = $('body');
+
+        for (var classIndex = 0; classIndex < displayClasses.length; classIndex++) {
+            body.removeClass('display-' + displayClasses[classIndex]).addClass('display-' + deriveDisplaySizeClass());
+        }
+
+        body.attr('data-display-class', deriveDisplaySizeClass().replace('display-', ''));
+    });
+
+    var getDisplayClass = function () {
+        return $('body').attr('data-display-class');
+    };
+
+    var getScrollOffset = function () {
+        var offsets = {
+            'upper-nav-affix': {
+                'lg': 300,
+                'md': 300,
+                'sm': 200,
+                'xs': 300
+            },
+            'default': {
+                'lg': 298,
+                'md': 298,
+                'sm': 198,
+                'xs': 298
+            }
+        };
+
+        var offsetGroup = $('body').is('.upper-nav-affix') ? offsets['upper-nav-affix'] : offsets['default'];
+        return offsetGroup[getDisplayClass()];
+    };
+
+    var getAffixOffset = function () {
+        var offsets = {
+            'lg': 278,
+            'md': 278,
+            'sm': 238,
+            'xs': 278
+        };
+
+        return offsets[getDisplayClass()];
+    };
+
+
     $('a[href*=#]:not([href=#])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
             var target = $(this.hash);
@@ -7,7 +66,9 @@ $(function() {
             var hashValue = this.hash.slice(1);
 
             if (target.length) {
-                var offset = $('body').is('.upper-nav-affix') ? 300 : 298;
+
+
+                var offset = getScrollOffset();
 
                 $('html,body').animate({
                     scrollTop: (target.offset().top) - offset
@@ -25,15 +86,44 @@ $(function() {
 
         if (target.length) {
             $('html,body').animate({
-                scrollTop: (target.offset().top) - 300
+                scrollTop: (target.offset().top) - getScrollOffset()
             }, 400);
         }
     }
+
+    var mostRecentlyClicked = null;
 
     $('#upper-nav').on('affix.bs.affix', function (event) {
         $('body').addClass('upper-nav-affix').removeClass('upper-nav-affix-top');
     }).on('affix-top.bs.affix', function (event) {
         $('body').removeClass('upper-nav-affix').addClass('upper-nav-affix-top');
+    }).on('activate.bs.scrollspy', function (event) {
+        if (mostRecentlyClicked == null) {
+            return false;
+        }
+
+        var target = $(event.target);
+
+        if (target.attr('id') === mostRecentlyClicked.attr('id')) {
+            mostRecentlyClicked = null;
+        } else {
+            target.removeClass('active');
+        }
+
+        return true;
+    });
+
+
+    $('#upper-nav a').click(function (event) {
+        var targetListItem = $(this).closest('li');
+        if (targetListItem.is('.active')) {
+            return false;
+        }
+
+        $('#upper-nav li').removeClass('active');
+        mostRecentlyClicked = targetListItem;
+
+        return false;
     });
 
 
@@ -42,6 +132,8 @@ $(function() {
         var target = $(hashValue);
 
         if (target.length) {
+            $(this).closest('li').addClass('selected');
+
             var offset = $('body').is('.upper-nav-affix') ? 300 : 298;
 
             $('html,body').animate({
@@ -51,6 +143,12 @@ $(function() {
             });
 
             return false;
+        }
+    });
+
+    $('#upper-nav').affix({
+        offset: {
+            top: getAffixOffset()
         }
     });
 });
