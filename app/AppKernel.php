c503d960621193b35858d7343d1973aad0a95840
Jon Cram <jon@webignition.net>
2017-07-03 19:37:19 +0100

    Upgrade to Symfony 2.7

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,10 +20,11 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new SimplyTestable\WebsiteBundle\SimplyTestableWebsiteBundle(),
-            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),            
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
