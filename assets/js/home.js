b28d889746e90446ef4fbae5ac66927be1990838
Jon Cram <webignition@gmail.com>
2017-12-18 11:59:13 +0000

    Test homepage js with tape (#64)

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,11 +1,11 @@
-module.exports = function () {
+module.exports = function (document, eventName) {
     const initialOffset = 310;
     const landingStrip = document.getElementById('landing-strip');
 
-    document.body.onscroll = function () {
+    document.addEventListener(eventName, function () {
         let offset = (Math.max(document.documentElement.scrollTop, document.body.scrollTop)) / 4;
         let updated = offset * -1 - initialOffset;
 
         landingStrip.style.backgroundPositionY = updated + 'px';
-    };
+    });
 };
