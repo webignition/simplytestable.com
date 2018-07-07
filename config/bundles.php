aabdce109df74a5330982b0e662431aa8bd210e9
Jon Cram <jon@webignition.net>
2018-07-07 21:04:05 +0100

    Install twig

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
