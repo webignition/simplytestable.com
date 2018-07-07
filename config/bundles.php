60eef28e7f30882f882d0949733e8ac2053c780a
Jon Cram <jon@webignition.net>
2018-07-07 20:33:55 +0100

    Run flex recipes

---
+++ config/bundles.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+];
