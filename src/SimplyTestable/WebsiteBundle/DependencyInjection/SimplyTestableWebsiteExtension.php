eb5e38044927e18f759dbd9dda466542ed4bfc27
Jon Cram <webignition@gmail.com>
2018-07-07 15:28:29 +0100

    Move plans, plan_distinctions (#89)

--- src/SimplyTestable/WebsiteBundle/DependencyInjection/SimplyTestableWebsiteExtension.php
+++ src/SimplyTestable/WebsiteBundle/DependencyInjection/SimplyTestableWebsiteExtension.php
@@ -4,7 +4,6 @@ namespace SimplyTestable\WebsiteBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Yaml\Yaml;
 
@@ -27,13 +26,5 @@ class SimplyTestableWebsiteExtension extends Extension
             __DIR__.'/../Resources/config',
             __DIR__.'/../Resources/config/plans',
         ]);
-
-        $loader = new Loader\YamlFileLoader($container, $fileLocator);
-
-        $planData = Yaml::parse(file_get_contents($fileLocator->locate('plans.yml')));
-        $planDistinctionData = Yaml::parse(file_get_contents($fileLocator->locate('distinctions.yml')));
-
-        $container->setParameter('plans', $planData);
-        $container->setParameter(('plan_distinctions'), $planDistinctionData);
     }
 }
