764c1b13c3ec526a505425e50854cb0adcf2a55d
Jon Cram <jon@webignition.net>
2018-07-08 20:33:07 +0100

    Remove unused files

--- tools/resetdb-dev.sh
+++
@@ -1,4 +0,0 @@
-echo "Resetting dev datbase"
-php app/console doctrine:database:drop -e dev --force
-php app/console doctrine:database:create -e dev
-php app/console doctrine:migrations:migrate -e dev --no-interaction
