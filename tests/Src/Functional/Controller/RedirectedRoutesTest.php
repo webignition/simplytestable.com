d47914e9509d3a69a588d398e9cf3878e38f31ad
Jon Cram <webignition@gmail.com>
2018-08-22 17:33:22 +0100

    Redirect /enterprise to /plans/enterprise (#112)

---
+++ tests/Src/Functional/Controller/RedirectedRoutesTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Tests\Src\Functional\Controller;
+
+use App\Tests\Src\Functional\AbstractWebTestCase;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectedRoutesTest extends AbstractWebTestCase
+{
+    /**
+     * @dataProvider redirectedRouteDataProvider
+     *
+     * @param string $path
+     * @param string $expectedRedirectUrl
+     */
+    public function testRedirectedRoute(string $path, string $expectedRedirectUrl)
+    {
+        $this->getCrawler([
+            'url' => $path,
+        ]);
+
+        /* @var RedirectResponse $response */
+        $response = $this->getClientResponse();
+
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+
+        $this->assertEquals(Response::HTTP_MOVED_PERMANENTLY, $response->getStatusCode());
+        $this->assertEquals($expectedRedirectUrl, $response->getTargetUrl());
+    }
+
+    /**
+     * @return array
+     */
+    public function redirectedRouteDataProvider()
+    {
+        return [
+            '/enterprise' => [
+                'path' => '/enterprise',
+                'expectedRedirectUrl' => 'http://localhost/plans/enterprise'
+            ],
+        ];
+    }
+}
